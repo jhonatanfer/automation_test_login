f47570d234786b1fa4aa8a9c6e34cf2b0ac41193
Jhonatan Fernandez <jhonatanfer@gmail.com>
2020-06-04 18:39:49 -0500

    primer commit

---
+++ new_proyect.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+ventana = tk.Tk()
+ventana.title("Primer ventana")
+#AnchoXAlto
+ventana.geometry('300x300')
+ventana.configure(background='dark turquoise')
+etiqueta1
+
+ventana.mainloop()
