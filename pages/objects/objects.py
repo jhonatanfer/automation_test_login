c5304cfdb2a958091ee7251469c02852c9681d02
Jhonatan Fernandez <jhonatanfer@gmail.com>
2020-07-25 20:05:33 -0500

    add report

--- pages/objects/objects.py
+++ pages/objects/objects.py
@@ -2,10 +2,10 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 
 class BasePageElement:
-    """Base page class that is initialized on every page object class."""
+
     locator = ""
+
     def __set__(self, obj, value):
-        """Sets the text to the value supplied"""
         driver = obj.driver
         WebDriverWait(driver, 100).until(
             lambda driver: driver.find_element_by_name(self.locator))
@@ -13,7 +13,6 @@ class BasePageElement:
         driver.find_element_by_name(self.locator).send_keys(value)
 
     def __get__(self, obj, owner):
-        """Gets the text of the specified object"""
         driver = obj.driver
         WebDriverWait(driver, 100).until(
             lambda driver: driver.find_element_by_name(self.locator))
