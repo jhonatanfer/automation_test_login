5e83201060d5f0c6aafa6ab62e5bb535c59aea1e
Jhonatan Fernandez <jhonatanfer@gmail.com>
2020-07-26 21:59:39 -0500

    t

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from pages.page_register import MainPage
 import unittest
+import time
+
 from selenium import webdriver
 
 
 class LoginTest(unittest.TestCase):
-    
+
     def setUp(self):
         self.driver = webdriver.Chrome(
             '/home/jhonatanf/Documentos/Automation/chromedriver')
@@ -18,24 +20,32 @@ class LoginTest(unittest.TestCase):
         self.driver.implicitly_wait(5)
         title = self.driver.find_element_by_tag_name("h2")
         assert title.text == "Servicios"
+        self.driver.save_screenshot(
+            '/home/jhonatanf/Imágenes/Test_aut_screeshots/test_successful_login.png')
 
     def test_fail_login_invalid_pwd(self):
         main = MainPage(self.driver)
         main.email_element = 'qa.automation.liftit@gmail.com'
         main.pwd_element = '$$L12345678'
         main.login()
-        self.driver.implicitly_wait(5)
+        self.driver.implicitly_wait(10)
         message = self.driver.find_element_by_id("notification1")
         assert message.text == "Email o contraseña inválidas"
+        time.sleep(1)
+        self.driver.save_screenshot(
+            '/home/jhonatanf/Imágenes/Test_aut_screeshots/test_fail_login_invalid_pwd.png')
 
     def test_fail_login_invalid_user(self):
         main = MainPage(self.driver)
         main.email_element = 'qa.automation$$$.liftit@gmail.com'
         main.pwd_element = 'L12345678'
         main.login()
-        self.driver.implicitly_wait(5)
+        self.driver.implicitly_wait(10)
         message = self.driver.find_element_by_id("notification1")
         assert message.text == "Email o contraseña inválidas"
+        time.sleep(1)
+        self.driver.save_screenshot(
+            '/home/jhonatanf/Imágenes/Test_aut_screeshots/test_fail_login_invalid_user.png')
 
     def tearDown(self):
         self.driver.close()
